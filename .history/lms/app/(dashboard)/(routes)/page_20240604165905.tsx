3cea828630b48b1e6ae6f608db8300cb9145557c
ireneryu <115036714+jhryu0706@users.noreply.github.com>
2024-06-05 15:24:12 -0500

    Delete .history directory

--- .history/lms/app/(dashboard)/(routes)/page_20240604165905.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-    <p className="text-3xl font-medium text-sky-700">
-      This is a protected folder
-    </p>
-    </div>
-  );
-}
--- .history/lms/app/(dashboard)/(routes)/page_20240605153421.tsx
+++
@@ -1,5 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-
-export default function Page() {
-  return <UserButton />;
-}
